03eef4ffabb9afa1b0fce69c36948dc9bb8a2cde
Dennis Bretz <dbretz@ceteris.ag>
2016-12-12 10:08:59 +0100

    - Added Create SSIS DB Catalog

---
+++ CSSISDB/Helper/New-CSSISDBCatalog.ps1
@@ -0,0 +1,49 @@
+﻿<#
+
+.SYNOPSIS
+TBD
+
+.DESCRIPTION
+TBD
+
+.NOTES
+Version 0.01
+Author Dennis Bretz
+#>
+function New-CSSISDBCatalog
+{
+    [cmdletBinding()]
+    param
+    (
+    	# TBD
+		[Parameter(Mandatory=$TRUE)]
+		[Microsoft.SqlServer.Management.Sdk.Sfc.SfcInstance]$IntegrationServices,
+
+        # TBD
+		[Parameter(Mandatory=$FALSE)]
+		[string]$SSISDBName = 'SSISDB',
+
+        # TBD
+		[Parameter(Mandatory=$TRUE)]
+		[string]$SSISDBPassword,
+		
+        # TBD
+        [Parameter(Mandatory=$FALSE)]
+        [string]$PartialName = 'Microsoft.SqlServer.Management.IntegrationServices.Catalog'
+
+
+    )
+    Begin{}
+
+    Process{
+        Write-Verbose -Message "Creating SSIS Catalog"
+        $Catalog = New-Object $PartialName ($IntegrationServices, $SSISDBName, $SSISDBPassword)
+        $Catalog.Create()
+		Write-Verbose -Message "Catalog was created"
+		return($Catalog)
+    }
+
+    End{}
+
+
+}
