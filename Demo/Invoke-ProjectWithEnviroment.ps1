3d00033b21757dc3d5190fb369de5745859c1bcc
Dennis Bretz <dbretz@ceteris.ag>
2016-12-15 14:59:46 +0100

    Edit Demo

--- Demo/Invoke-ProjectWithEnviroment.ps1
+++ Demo/Invoke-ProjectWithEnviroment.ps1
@@ -2,20 +2,24 @@
 $Config = ([xml](Get-Content -Path "M:\Projekte\CSSISDB\CSSISDB\CSSISDB.config.xml" -ErrorAction Stop)).Config  
 
 
+
+#C:\Windows\assembly\GAC_MSIL\Microsoft.SqlServer.Management.IntegrationServices\13.0.0.0__89845dcd8080cc91\Microsoft.SqlServer.Management.IntegrationServices.dll
 [System.Reflection.Assembly]::LoadWithPartialName($Config.General.PartialName) | Out-Null;
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection $Config.General.SqlConnectionString
 $IntegrationServicesObject = New-Object "$($Config.General.PartialName).IntegrationServices" $SqlConnection
 
-$FolName = $Config.SSISDB.Folders.Folder[0].Name #Folder1 
-Write-Host "Invoke Folder $($FolName)"
-$Folder = Invoke-CSSISDBFolder -FolderName $FolName -Verbose -IntegrationServicesObject $IntegrationServicesObject
+$ConfigFolder = $Config.SSISDB.Folders.Folder | Where-Object "Name" -Like "DBR_Powershell" |select -First 1
+$ConfigEnviroment = $Config.SSISDB.Enviroments.Enviroment | Where-Object "Name" -Like "DBR_Dev" | select -First 1
+$ConfigProjects = $ConfigFolder.Projects.Project
 
 
-$EnvName = $Config.SSISDB.Enviroments.Enviroment[0].Name #DEV
-$ProName = $Config.SSISDB.Folders.Folder[0].Projects.Project[0].Name #Project1
-Write-Host "$($ProName) in $($FolName) will get an Enviroment $($EnvName)"
-Invoke-CSSISDBEnviromentProjectVariable -IntegrationServices $IntegrationServicesObject -FolderName $FolName -EnviromentName $EnvName -ProjectName $ProName -Verbose
+Write-Host "Invoke Folder $($ConfigFolder.Name)"
+$Folder = Invoke-CSSISDBFolder -FolderName $ConfigFolder.Name -Verbose -IntegrationServicesObject $IntegrationServicesObject
 
 
-$ProName = $Config.SSISDB.Folders.Folder[0].Projects.Project[1].Name #Project2
-Invoke-CSSISDBEnviromentProjectVariable -IntegrationServices $IntegrationServicesObject -FolderName $FolName -EnviromentName $EnvName -ProjectName $ProName -Verbose
+
+
+Foreach($ConfigProject in $ConfigProjects){
+    Write-Host "$($ConfigProject.Name) in $($Folder.Name) will get an Enviroment $($ConfigEnviroment.Name)"
+    Invoke-CSSISDBEnviromentProjectVariable -IntegrationServices $IntegrationServicesObject -FolderName $Folder.Name -EnviromentName $ConfigEnviroment.Name -ProjectName $ConfigProject.Name -Verbose
+}
