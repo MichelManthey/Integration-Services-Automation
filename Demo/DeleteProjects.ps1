a00d19564ffc6243518fedee42df9f268b1ae627
Dennis Bretz <dbretz@ceteris.ag>
2017-01-03 15:50:22 +0100

    - Überarbeitung Demo Delete Folder

--- Demo/DeleteProjects.ps1
+++ Demo/DeleteProjects.ps1
@@ -1,10 +1,13 @@
-﻿Import-Module 'M:\Projekte\CIsa\CIsa' -Verbose -Force
-$Config = ([xml](Get-Content -Path "M:\Projekte\CIsa\CIsa\CIsa.config.xml" -ErrorAction Stop)).Config  
+﻿$ModulePath = "D:\Users\DBR\Projects\Powershell\Cisa\CIsa"
+$ConfigPath = "$($ModulePath)\CIsa.config.simple.xml"
+
+Import-Module $ModulePath -Verbose -Force
+$Config = ([xml](Get-Content -Path $Path -ErrorAction Stop)).Config
 
 
-[System.Reflection.Assembly]::LoadWithPartialName($Config.General.PartialName) | Out-Null;
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Management.IntegrationServices") | Out-Null;
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection $Config.General.SqlConnectionString
-$IntegrationServices = New-Object "$($Config.General.PartialName).IntegrationServices" $SqlConnection
+$IntegrationServices = New-Object "Microsoft.SqlServer.Management.IntegrationServices.IntegrationServices" $SqlConnection
 
 
 Foreach($ConfigFolder in $Config.SSISDB.Folders.Folder){
